030d3f9d86d23418d5b3700741fcfe7a3a8c9d2f
liuyj <429593717@qq.com>
2021-08-04 00:26:02 +0800

    resolve cors

--- router/route.go
+++ router/route.go
@@ -65,6 +65,7 @@ func InitRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
 
 	router := gin.Default()
 	router.Use(middlewares...)
+	router.Use(middleware.Cors())
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
